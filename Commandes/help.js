a710302b00f699a6b4d07fe88100b12839a6aa4a
tanguybron <63880113+tanguybron@users.noreply.github.com>
2020-07-11 14:03:09 +0200

    help vote

--- Commandes/help.js
+++ Commandes/help.js
@@ -15,6 +15,10 @@ module.exports.run = async(client, message, args) => {
                 name : "> Commandes : ",
                 value : "\n help pour afficher l'aide \n \n clear <nb> pour effacer les 'nb' derniers messages du channel \n \n stats @mention pour avoir les statistiques du compte (si pas de mention, s'affiche les stats de celui qui a appelé la commande) \n \n ping qui calcule le temps de réponse du serveur.",
                 },
+                {
+                name : "> votes : ",
+                value : "\n [arg1] : prop1, prop2, prop3,... start, results \n exemple : !!vote prop1 premier : cette commande attribut 'premier' comme première possibilité de vote.",
+                },
             ],
         }
     })
